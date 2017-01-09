b05c07cd879dcb58a67a2f5781af4fff8367b012
Maksim Kostromin <daggerok@gmail.com>
2017-01-09 19:44:42 +0200

    update jenkins

--- jenkins/jenkins.bash
+++ jenkins/jenkins.bash
@@ -5,13 +5,24 @@ docker rm jenkins
 docker pull jenkins:$JENKINS_VERSION
 
 # docker run -d \
-#   -name jenkins \
-#   -p 2222:22 \
+#   -name ci-jenkins \
 #   -p 8080:8080 \
 #   -p 50000:50000 \
-#   -v $PWD/jenkins/data:/var/jenkins_home \
+#   -v $PWD/var/jenkins_home:/var/jenkins_home \
 #   jenkins:$JENKINS_VERSION
 
-docker run --name jenkins --publish 22:2222 --publish 8080:8080 --publish 50000:50000 -v $PWD/jenkins/data:/var/jenkins_home jenkins:$JENKINS_VERSION
+docker run --name ci-jenkins -d -p 8080:8080 -p 50000:50000 -v $PWD/var/jenkins_home:/var/jenkins_home jenkins:$JENKINS_VERSION
+# docker run --name ci-jenkins -p 8080:8080 -p 50000:50000 -v $PWD/var/jenkins_home:/var/jenkins_home jenkins:$JENKINS_VERSION
 
-# docker exec -it jenkins bash
+# docker exec -it ci-jenkins bash
+# docker exec -u root -it ci-jenkins bash
+
+# rm -rf $PWD/var/jenkins_home
+# docker stop ci-jenkins
+# docker rm -f ci-jenkins
+# vim Dockerfile # update user passsword and id_rsa.pub key
+# docker build -t ci-jenkins .
+# docker run --name ci-jenkins -p 8080:8080 -p 50000:50000 -v $PWD/var/jenkins_home:/var/jenkins_home ci-jenkins
+
+# docker stop ci-jenkins
+# docker start ci-jenkins
