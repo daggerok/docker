8c8bfaf605e3cf5729f65d1c6ed14715694f9177
Maksim Kostromin <daggerok@gmail.com>
2017-01-09 17:29:39 +0200

    add gitlab

---
+++ gitlab/gitlab.bash
@@ -0,0 +1,16 @@
+export GITLAB_VERSION="8.15.3-ce.0"
+
+docker rm gitlab
+docker pull gitlab/gitlab-ce:$GITLAB_VERSION
+
+# docker run --detach \
+#   --publish 443:443 --publish 80:80 --publish 22:22 \
+#   --name gitlab \
+#   --volume $PWD/gitlab/config:/etc/gitlab \
+#   --volume $PWD/gitlab/logs:/var/log/gitlab \
+#   --volume $PWD/gitlab/data:/var/opt/gitlab \
+#   gitlab/gitlab-ce:$GITLAB_VERSION
+
+docker run --detach --publish 443:443 --publish 80:80 --publish 22:22 --name gitlab --volume $PWD/gitlab/data:/var/opt/gitlab gitlab/gitlab-ce:$GITLAB_VERSION
+
+# docker exec -it gitlab bash
