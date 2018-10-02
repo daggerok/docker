ccb27ef77954efc8b8c61578ea9f1b0a448e8b53
Maksim Kostromin <daggerok@gmail.com>
2018-10-02 23:58:15 +0300

    Create start-oracle-xe.bash

---
+++ oracle/start-oracle-xe.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker container run -it -e ORACLE_PWD=password -p 8080:8080 -p 1521:1521 --name oracle --shm-size=1g daggerok/oracle:xe
+#docker container stop oracle
+#docker container run -ia oracle
