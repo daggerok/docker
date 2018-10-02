8c57baf3736683067ccd73a0089665100399b7eb
Maksim Kostromin <daggerok@gmail.com>
2018-10-03 02:05:26 +0300

    Update daggerok-11xe.bash

--- oracle/daggerok-11xe.bash
+++ oracle/daggerok-11xe.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
-docker container run -it -e ORACLE_PWD=password -p 8080:8080 -p 1521:1521 --name oracle --shm-size=1g daggerok/oracle:xe
+docker container run -it -p 8080:8080 -p 1521:1521 --name oracle-xe --shm-size=1g daggerok/oracle-xe
 #docker container stop oracle
 #docker container run -ia oracle
+##docker container run -it -e ORACLE_PWD=password -p 8080:8080 -p 1521:1521 --name oracle --shm-size=1g daggerok/oracle:xe
